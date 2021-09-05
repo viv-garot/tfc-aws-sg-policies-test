bfd45c22ef438d141f06d269fd24d26179ab2481
viv-garot <vivien@hashicorp.com>
2021-09-05 20:09:36 +0200

    added main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vivien-vpc"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "vivien-allow_sss"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  egress {
+    description      = "SSH to VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
